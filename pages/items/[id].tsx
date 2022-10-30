12646a88597804a33b8c6440fa9f169e373c85a6
daewoongBang <daewoong.bang@gmail.com>
2022-10-30 15:09:36 +0900

    Test Commit

--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -25,14 +25,7 @@ const ItemDetail: NextPage = () => {
             <span className='text-3xl block mt-3'>$140</span>
 
             <p className='text-base my-6 text-gray-700'>
-              My money&apos;s in that office, right? If she start giving me some
-              bullshit about it ain&apos;t there, and we got to go someplace
-              else and get it, I&apos;m gonna shoot you in the head then and
-              there. Then I&apos;m gonna shoot that bitch in the kneecaps, find
-              out where my goddamn money is. She gonna tell me too. Hey, look at
-              me when I&apos;m talking to you, motherfucker. You listen: we go
-              in there, and that ni**a Winston or anybody else is in there, you
-              the first motherfucker to get shot. You understand?
+              My money&apos;s in that office, right? You understand?
             </p>
 
             <div className='flex items-center justify-between space-x-2'>
