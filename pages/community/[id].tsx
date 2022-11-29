8c0902b5bebdbe114f5cc0caaa0fe9895c3d0d3d
daewoongBang <daewoong.bang@gmail.com>
2022-11-29 18:21:58 +0900

    Add Community - Post Feature

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -1,7 +1,38 @@
 import type { NextPage } from 'next';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+import Link from 'next/link';
+
 import Layout from '@components/layout';
+import TextArea from '@components/textarea';
+import { Answer, Post, User } from '@prisma/client';
+
+interface AnswerWithUser extends Answer {
+  user: User;
+}
+
+interface PostWithUser extends Post {
+  user: User;
+  answers: AnswerWithUser[];
+  _count: {
+    answers: number;
+    wonderings: number;
+  };
+}
+
+interface CommunityPostResponse {
+  ok: boolean;
+  post: PostWithUser;
+}
 
 const CommunityPostDetail: NextPage = () => {
+  const { register } = useForm();
+  const router = useRouter();
+  const { data, error } = useSWR<CommunityPostResponse>(
+    router.query.id ? `/api/posts/${router.query.id}` : null
+  );
+
   return (
     <Layout canGoBack>
       <div>
@@ -13,18 +44,22 @@ const CommunityPostDetail: NextPage = () => {
           <div className='w-10 h-10 rounded-full bg-slate-300' />
 
           <div>
-            <p className='text-sm font-medium text-gray-700'>Steve Jebs</p>
-
-            <p className='text-xs font-medium text-gray-500'>
-              View profile &rarr;
+            <p className='text-sm font-medium text-gray-700'>
+              {data?.post.user.name}
             </p>
+
+            <Link href={`/users/profiles/${data?.post.user?.id}`}>
+              <p className='text-xs font-medium text-gray-500'>
+                View profile &rarr;
+              </p>
+            </Link>
           </div>
         </div>
 
         <div>
           <div className='mt-2 px-4 text-gray-700'>
-            <span className='text-orange-500 font-medium'>Q.</span> What is the
-            best mandu restaurant?
+            <span className='text-orange-500 font-medium'>Q.</span>{' '}
+            {data?.post?.question}
           </div>
 
           <div className='flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-[2px]  w-full'>
@@ -44,7 +79,7 @@ const CommunityPostDetail: NextPage = () => {
                 ></path>
               </svg>
 
-              <span>궁금해요 1</span>
+              <span>궁금해요 {data?.post._count.wonderings}</span>
             </span>
 
             <span className='flex space-x-2 items-center text-sm'>
@@ -63,34 +98,37 @@ const CommunityPostDetail: NextPage = () => {
                 ></path>
               </svg>
 
-              <span>답변 1</span>
+              <span>답변 {data?.post._count.answers}</span>
             </span>
           </div>
         </div>
 
         <div className='px-4 my-5 space-y-5'>
-          <div className='flex items-start space-x-3'>
-            <div className='w-8 h-8 bg-slate-200 rounded-full' />
+          {data?.post.answers.map(answer => (
+            <div key={answer.id} className='flex items-start space-x-3'>
+              <div className='w-8 h-8 bg-slate-200 rounded-full' />
 
-            <div>
-              <span className='text-sm block font-medium text-gray-700'>
-                Steve Jebs
-              </span>
+              <div>
+                <span className='text-sm block font-medium text-gray-700'>
+                  {answer.user.name}
+                </span>
 
-              <span className='text-xs text-gray-500 block '>2시간 전</span>
+                <span className='text-xs text-gray-500 block '>
+                  {String(answer.createdAt)}
+                </span>
 
-              <p className='text-gray-700 mt-2'>
-                The best mandu restaurant is the one next to my house.
-              </p>
+                <p className='text-gray-700 mt-2'>{answer.answer}</p>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
 
         <div className='px-4'>
-          <textarea
-            className='mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500 '
-            rows={4}
+          <TextArea
+            register={register('description', { required: true, minLength: 2 })}
+            name='description'
             placeholder='Answer this question!'
+            required
           />
 
           <button className='mt-2 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none '>
--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -1,15 +1,50 @@
 import type { NextPage } from 'next';
+import { useForm } from 'react-hook-form';
+
 import Button from '@components/button';
 import Layout from '@components/layout';
 import TextArea from '@components/textarea';
+import useMutation from '@libs/client/useMutation';
+import { Post } from '@prisma/client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+interface WriteForm {
+  question: string;
+}
+
+interface WriteResponse {
+  ok: boolean;
+  post: Post;
+}
 
 const Write: NextPage = () => {
+  const { register, handleSubmit } = useForm<WriteForm>();
+  const [post, { loading, data }] = useMutation<WriteResponse>('/api/posts');
+  const router = useRouter();
+
+  const onValid = (data: WriteForm) => {
+    if (loading) return;
+
+    post(data);
+  };
+
+  useEffect(() => {
+    if (data && data.ok) {
+      router.push(`/community/${data.post.id}`);
+    }
+  }, [data, router]);
+
   return (
     <Layout title='Write Post' canGoBack>
-      <form className='py-4 space-y-4'>
-        <TextArea required placeholder='Ask a question!' />
+      <form onSubmit={handleSubmit(onValid)} className='py-4 space-y-4'>
+        <TextArea
+          register={register('question', { required: true, minLength: 5 })}
+          required
+          placeholder='Ask a question!'
+        />
 
-        <Button text='Submit' />
+        <Button text={loading ? 'Loading...' : 'Submit'} />
       </form>
     </Layout>
   );
