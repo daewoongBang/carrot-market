51e09c302697fb4977a1c0a760079c71e83dd257
daewoongBang <daewoong.bang@gmail.com>
2023-01-02 16:23:43 +0900

    Change Login Title

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -66,7 +66,7 @@ const Enter: NextPage = () => {
 
   return (
     <div className='mt-16 p-4'>
-      <h3 className='text-3xl font-bold text-center'>Enter to Carrot</h3>
+      <h3 className='text-3xl font-bold text-center'>Carrot Market</h3>
 
       <div className='mt-8'>
         {data?.ok ? (
